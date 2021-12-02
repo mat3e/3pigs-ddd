5303d6d1eb5465b5540ce86c1403e5451ea5e3c2
Mateusz Chrzonstowski <mateusz@brightcube.eu>
2021-12-02 20:50:19 +0100

    Put adapter requirements in JavaDoc

--- domain/src/main/java/io/github/mat3e/model/HouseRepository.java
+++ domain/src/main/java/io/github/mat3e/model/HouseRepository.java
@@ -3,5 +3,8 @@ package io.github.mat3e.model;
 import io.github.mat3e.ddd.DomainRepository;
 import io.github.mat3e.model.vo.HouseId;
 
+/**
+ * Needs to publish house events.
+ */
 public interface HouseRepository extends DomainRepository<HouseId, House> {
 }
