b8f960eb0ed7c670437e1199568a86117ea5c8be
mat3e <mateusz@brightcube.eu>
2022-05-12 19:46:17 +0200

    Add bad requests handling for DELETE

--- adapters/src/main/java/io/github/mat3e/in/rest/ThreePigsController.java
+++ adapters/src/main/java/io/github/mat3e/in/rest/ThreePigsController.java
@@ -1,5 +1,6 @@
 package io.github.mat3e.in.rest;
 
+import io.github.mat3e.app.HouseNotFoundException;
 import io.github.mat3e.app.HouseQueryRepository;
 import io.github.mat3e.app.HouseReadModel;
 import io.github.mat3e.app.ThreePigsCommandHandler;
@@ -9,13 +10,7 @@ import io.github.mat3e.model.vo.HouseId;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
@@ -50,8 +45,16 @@ class ThreePigsController {
     }
 
     @DeleteMapping("/{value}")
-    ResponseEntity<Void> blowHouseDown(@PathVariable("value") HouseId id) {
-        handler.handle(new BlowDown(id));
-        return ResponseEntity.noContent().build();
+    ResponseEntity<?> blowHouseDown(@PathVariable("value") HouseId id) {
+        HouseId updatedId = handler.handle(new BlowDown(id));
+        return repository.findDirect(updatedId)
+                .filter(house -> !house.isDestroyed())
+                .map(house -> ResponseEntity.badRequest().body("Cannot destroy house from " + house.getMaterial() + " with pigs: " + house.getPigs()))
+                .orElse(ResponseEntity.noContent().build());
+    }
+
+    @ExceptionHandler(HouseNotFoundException.class)
+    ResponseEntity<Void> handle() {
+        return ResponseEntity.notFound().build();
     }
 }
